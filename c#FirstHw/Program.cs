ba655b7f6dcc0be60bc7639e3152ccb9084a5cd2
Natalia <natalyaivvonen@gmail.com>
2022-06-24 14:44:49 +0300

    first task solved

---
+++ c#FirstHw/Program.cs
@@ -0,0 +1,17 @@
+﻿int num1, num2;
+
+Console.Write("Input first number: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num2 > num1)
+{
+    Console.Write("max number is " + num2 + ", ");
+    Console.Write("min number is " + num1 + " ");
+}
+else
+{
+    Console.Write("max number is " + num1 + ", ");
+    Console.Write("min number is " + num2 + " ");
+}
