56651fa6da4461981a226e8c65c313a99214b8f1
Natalia <natalyaivvonen@gmail.com>
2022-06-30 22:04:36 +0300

    bugFix

--- SecondHw/Program.cs
+++ SecondHw/Program.cs
@@ -1,50 +1,52 @@
 ﻿// task 1
-/*
-int SecondNumber()
-{
-    int num = new Random().Next(100, 1000);
-    Console.WriteLine("Current number is " + num);
-    
-    int dec = num / 10;
-    int result = dec % 10;
-    return result;
-    
-}
 
-int number = SecondNumber();
-Console.WriteLine(number);
+// int SecondNumber(int num)
+// {
+    
+    
+//     int dec = num / 10;
+//     int result = dec % 10;
+//     return result;
+    
+// }
+// int number = new Random().Next(100, 1000);
+// Console.WriteLine("Current number is " + number);
 
-*/
+// Console.WriteLine(SecondNumber(number));
+
 
 // task 2
 
-void ThirdNum()
-{
-    int num;
-
-    Console.Write("Input a number: ");
-    num = Convert.ToInt32(Console.ReadLine());
-  
-        while(num < 100000000)
-        {
-            if(num < 1000 && num > 99)
-            {
-                int tis = num % 10;
-                Console.WriteLine(tis);
-                break;
-            }
-            else
-            {
-                num = num / 10;
-            }
+// void ThirdNum(int num)
+// {
+ 
+//         while(num < 1000000000)
+//         {
+//             if(num < 99) 
+//             {
+//                 Console.WriteLine("Третьей цифры нет!");
+//                 break;
+//             }
+//             if(num < 1000 && num > 99)
+//             {
+//                 int tis = num % 10;
+//                 Console.WriteLine(tis);
+//                 break;
+//             }
+//             else
+//             {
+//                 num = num / 10;
+//             }
             
-        }
+//         }
                  
             
             
-}
+// }
 
-ThirdNum();
+// Console.Write("Input a number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// ThirdNum(number);
 
 
 
@@ -52,32 +54,18 @@ ThirdNum();
 
 
 // task 3
-/*
-bool DayNumber()
+
+bool DayNumber(int DayNum)
 {
-    int DayNum;
-
-    Console.Write("Input a day number: ");
-    DayNum = Convert.ToInt32(Console.ReadLine());
-
-   if(DayNum == 6)
-   {
+     
+   if(DayNum == 6 || DayNum == 7) 
         return true;
-   }
     else
-    {
-        if (DayNum == 7)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
+        return false;
     
 }
 
-Console.WriteLine(DayNumber());
+Console.Write("Input a day number: ");
+  int DayOfWeek = Convert.ToInt32(Console.ReadLine());
 
-*/
+Console.WriteLine(DayNumber(DayOfWeek));
